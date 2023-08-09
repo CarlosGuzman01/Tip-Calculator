e79fef6d239fffbd69e6d3ea080eafb522544de0
CarlosGuzman01 <carlosguzmantejeda13@gmail.com>
2023-08-08 20:47:08 -0400

    done with the mvp

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.tipcalculator
 
+import android.animation.ArgbEvaluator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.TextureView
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.widget.EditText
 import android.widget.SeekBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
+private const val TAG = "MainActivity"
+private const val INITIAL_TIP_PERCENT = 15
 class MainActivity : AppCompatActivity() {
 
     private lateinit var etBaseAmount: EditText
@@ -14,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var tvTipPercentLabel: TextView
     private lateinit var tvTipAmount: TextView
     private lateinit var tvTotalAmount: TextView
+    private lateinit var tvTipDescription: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,10 +31,20 @@ class MainActivity : AppCompatActivity() {
         tvTipPercentLabel = findViewById(R.id.tvTipPercentLabel)
         tvTipAmount = findViewById(R.id.tvTipAmount)
         tvTotalAmount = findViewById(R.id.tvTotalAmount)
+        tvTipDescription = findViewById(R.id.tvTipDescription)
+
+        seekBarTip.progress = INITIAL_TIP_PERCENT
+        tvTipPercentLabel.text = "$INITIAL_TIP_PERCENT%"
+
+        updateTipDescription(INITIAL_TIP_PERCENT)
+
 
         seekBarTip.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
-            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-                Log.i(TAG, )
+            override fun onProgressChanged(p0: SeekBar?, progress: Int, fromUser: Boolean) {
+                Log.i(TAG, "onProgressChanged $progress" )
+                tvTipPercentLabel.text = "$progress%"
+                computeTipAndTotal()
+                updateTipDescription(progress)
             }
 
             override fun onStartTrackingTouch(p0: SeekBar?) {}
@@ -35,7 +52,58 @@ class MainActivity : AppCompatActivity() {
             override fun onStopTrackingTouch(p0: SeekBar?) {}
 
         })
+        etBaseAmount.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                Log.i(TAG, "afterTextChanged $s")
+                computeTipAndTotal()
+            }
+
+        } )
+
+
+
+    }
+
+    private fun updateTipDescription(tipPercent: Int) {
+       val tipDescription = when (tipPercent){
+           in 0..9 -> "Poor"
+           in 10..14 -> "Acceptable"
+           in 15..19 -> "Good"
+           in 20..24 -> "Great"
+           else -> "Amazing"
+       }
+        tvTipDescription.text = tipDescription
+        //update the color based on the tipPercent
+        val color = ArgbEvaluator().evaluate(
+            tipPercent.toFloat() / seekBarTip.max,
+            ContextCompat.getColor(this, R.color.worst_tip),
+            ContextCompat.getColor(this, R.color.best_tip)
+        )as Int
+
+        tvTipDescription.setTextColor(color)
+    }
+
+    private fun computeTipAndTotal() {
+        //1. Get the value of the base and tip percent
+        if(etBaseAmount.text.isEmpty()){
+            tvTipAmount.text = ""
+            tvTotalAmount.text = ""
+            return
+        }
+        val baseAmout = etBaseAmount.text.toString().toDouble()
+        val tipPercent = seekBarTip.progress
+
+        //2. Compute the tip and total
+        val tipAmount = baseAmout * tipPercent/ 100
+        val totalAmount = baseAmout + tipAmount
+
+        //3. Update the UI
+        tvTipAmount.text = "%.2f".format(tipAmount)
+        tvTotalAmount.text ="%.2f".format(totalAmount)
 
 
     }
